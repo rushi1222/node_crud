41c87d1805056cfe4b277f221a1e853c5ab2ad95
vivekbodar <vivekbodar456@gmail.com>
2022-08-07 23:09:00 +0530

    Done Search logic in Node ejs
    done search api in node js for ejs.

--- server/routes/router.js
+++ server/routes/router.js
@@ -28,5 +28,6 @@ route.post("/api/users", controller.create);
 route.get("/api/users", controller.find);
 route.put("/api/users/:id", controller.update);
 route.delete("/api/users/:id", controller.delete);
+route.get('/search', controller.search)
 
 module.exports = route;
